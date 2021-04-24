a4f863953e08cc00c6054fff5c095043163270ae
Tatia burdett <tatiaburdett@gmail.com>
2021-04-24 10:18:16 -0700

    basic navbar styling

--- src/NavBar/NavBar.js
+++ src/NavBar/NavBar.js
@@ -4,8 +4,14 @@ import './NavBar.css'
 class NavBar extends React.Component {
   render() {
     return (
-      <div>
-        Hello NavBar
+      <div className='navbar-container'>
+        <h1 className='navbar-header'>TB</h1>
+        <ul className='navbar-menu'>
+          <li className='navbar-list-item'>About</li>
+          <li className='navbar-list-item'>Projects</li>
+          <li className='navbar-list-item'>Photography</li>
+          <li className='navbar-list-item'>Contact</li>
+        </ul>
       </div>
     )
   }
