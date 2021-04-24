b14acbdbe93210746aa09509bb22a7002f1c9a36
Tatia burdett <tatiaburdett@gmail.com>
2021-04-24 13:50:44 -0700

    sections styling

--- src/About/About.js
+++ src/About/About.js
@@ -1,18 +1,29 @@
 import React from 'react'
+import './About.css'
 
 class About extends React.Component {
   render() {
     return (
       <div className='about-container'>
-        <h2>About Me</h2>
-        <section className='about-section'>
-          <h3>Software Developer</h3>
-          <p>Why I choose to become a developer...</p>
-          <h3>Travel</h3>
-          <p>My love to travel...</p>
-          <h3>Philosophy on Life</h3>
-          <p>My passion to create a difference in the world, through art, design, technology, or just conversation</p>
-        </section>
+        <h1>About Me</h1>
+        <div className='about-sections-container'>
+
+          <section className='about-section'>
+            <h2>Software Developer</h2>
+            <p>Why I choose to become a developer...</p>
+          </section>
+
+          <section className='about-section'>
+            <h2>Travel</h2>
+            <p>My love to travel...</p>
+          </section>
+
+          <section className='about-section'>
+            <h2>Philosophy on Life</h2>
+            <p>My passion to create a difference in the world, through art, design, technology, or just conversation</p>
+          </section>
+
+        </div>
       </div>
     )
   }
