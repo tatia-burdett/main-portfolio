d7ac3070277031d31782068f2e9a9f16bbf8e3df
Tatia burdett <tatiaburdett@gmail.com>
2021-04-24 10:31:23 -0700

    add home component, positioning

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom'
+import './App.css'
 
 import NavBar from './NavBar/NavBar'
+import HomePage from './HomePage/HomePage'
 
-function App() {
-  return (
-    <div>
-      <nav>
-        <NavBar />
-      </nav>
-    </div>
-  );
+class App extends React.Component {
+
+  renderRoutes () {
+    return (
+      <Switch>
+        <Route 
+          exact
+          path='/'
+          component={HomePage}
+        />
+      </Switch>
+    )
+  }
+
+  render() {
+    return (
+      <div>
+        <nav>
+          <NavBar />
+        </nav>
+        <main>
+          {this.renderRoutes()}
+        </main>
+      </div>
+    )
+  }
 }
 
 export default App;
