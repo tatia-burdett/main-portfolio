7347f03fa02622c1a4445a288caf0c5f212075d8
Tatia burdett <tatiaburdett@gmail.com>
2021-04-24 13:37:20 -0700

    add hamburger menu

--- src/NavBar/NavBar.js
+++ src/NavBar/NavBar.js
@@ -6,21 +6,17 @@ class NavBar extends React.Component {
   render() {
     return (
       <div className='navbar-container'>
-        <h1><Link to='/' className='navbar-header'>TB</Link></h1>
-        <ul className='navbar-menu'>
-          <li className='navbar-list-item'>
+        <div className='navbar-hamburger'>
+          <label htmlFor='hamburger'>&#9776;</label>
+          <input type="checkbox" id="hamburger"/>
+          <div className='navbar-hamburger-items'>
+            <Link to='/'>Home</Link>
             <Link to='/about'>About</Link>
-          </li>
-          <li className='navbar-list-item'>
             <Link to='/projects'>Projects</Link>
-          </li>
-          <li className='navbar-list-item'>
             <Link to='/photography'>Photography</Link>
-          </li>
-          <li className='navbar-list-item'>
             <Link to='/contact'>Contact</Link>
-          </li>
-        </ul>
+          </div>
+        </div>
       </div>
     )
   }
