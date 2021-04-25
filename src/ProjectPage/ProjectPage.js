4ef2a57dcac0071f223e08a21319ab4ec20d0cb0
Tatia burdett <tatiaburdett@gmail.com>
2021-04-25 14:50:23 -0700

    change == to ===

--- src/ProjectPage/ProjectPage.js
+++ src/ProjectPage/ProjectPage.js
@@ -19,7 +19,7 @@ class ProjectPage extends React.Component {
     const { id } = this.props.match.params
     const idInt = parseInt(id)
 
-    const proj = project.find(project => project.id == idInt)
+    const proj = project.find(project => project.id === idInt)
 
     return (
       <div className='project-page-container'>
