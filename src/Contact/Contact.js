08e60861e3893d43e8fcd221bbb59a88ed3b4570
Tatia burdett <tatiaburdett@gmail.com>
2021-04-25 12:19:43 -0700

    add fonts

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -7,10 +7,17 @@ class Contact extends React.Component {
       <div className='contact-container'>
         <h1>Contact</h1>
         <section className='contact-section'>
-          <form>
-            <label>Contact Me</label>
-            <input />
-            <button>Submit</button>
+          <form action="https://formspree.io/f/xzbkaken " method="POST">
+            <label htmlFor="user-name">Name:</label>
+            <input id="user-name" type="text" name="name" placeholder="Enter your name" />
+
+            <label htmlFor="user-email">Email:</label>
+            <input id="user-email" type="text" name="email" placeholder="Enter your email" />
+
+            <label htmlFor="user-message">Message:</label>
+            <textarea id="user-message" name="message" placeholder="Enter a short message" />
+
+            <button type="submit">Submit</button>
           </form>
         </section>
       </div>
